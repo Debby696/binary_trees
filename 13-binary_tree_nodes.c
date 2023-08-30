88e8a23119e2d8a0175ab98ed378c376019c8765
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-08-29 21:03:46 -0700

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,3 +6,17 @@
  * Return: 0 if tree is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	nodes += tree->right || tree->left ? 1 : 0;
+
+	nodes += binary_tree_nodes(tree->left);
+
+	nodes += binary_tree_nodes(tree->right);
+
+	return (nodes);
+}
