968297b6c09e7a616c94fbaad425666564cb7e03
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-08-29 22:12:20 -0700

    fixed description error

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_is_full - Checks if a binary tree is full
+ * binary_tree_is_full - Checks if a binary tree is full
  * @tree: Binary tree
  *
  * Return: 1 or 0
