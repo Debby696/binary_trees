a1ef3daf20f8e1979f1413a9f4aece830a5a0c53
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-08-29 19:12:25 -0700

    corrected task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,12 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
 	/*calculate height of both left & right subtrees */
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	int left_height = binary_tree_height(tree->left);
+	int right_height = binary_tree_height(tree->right);
 
 	/* return the calculated height + the current node */
-	return ((left_height > right_height) ? left_height + 1 : right_height + 1);
+	return ((left_height > right_height) ? left_height + 1: right_height + 1);
 }
