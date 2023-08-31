0a1cae69d072e19d850e353d1918100b6ce6198b
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-08-31 08:11:32 -0700

    perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,8 +14,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	int nodes = count_nodes(tree);
+	int height = binary_tree_height(tree);
 
-	return ((nodes & (nodes + 1)) == 0);
+	return (((1 << (height + 1)) - 1) == nodes);
 }
 
 /**
@@ -30,4 +31,25 @@ int count_nodes(const binary_tree_t *tree)
 		return (1 + count_nodes(tree->left) + count_nodes(tree->right));
 
 	return (0);
+}
+
+
+/**
+ * binary_tree_height - this measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * Return: height of the tree or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	/*calculate height of both left & right subtrees */
+	left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+
+	/* return the calculated height + the current node */
+	return ((left > right) ? left : right);
 }
